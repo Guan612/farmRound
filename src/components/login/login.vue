dfa848bbf2529e2686094e90273542253f83d4d6
Guan612 <tiantony53@163.com>
2023-10-12 17:22:57 +0800

    Update login.vue

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="login">
     <h1>欢迎您。欢迎来到xxx</h1>
-    <n-input type="text" placeholder="电话号码" clearable v-model='telphone' />
-    <n-input type="password" placeholder="密码" v-model="password"></n-input>
+    <n-input type="text" placeholder="电话号码" clearable v-model='userInifo.telphone' />
+    <n-input type="password" placeholder="密码" v-model="userInifo.password"></n-input>
     <n-button @click="login">登录测试</n-button>
     </div>
 </template>
@@ -10,11 +10,11 @@
 <script setup>
     import { NButton,NInput} from 'naive-ui'
     import { zhCN, dateZhCN } from 'naive-ui'
-    import { reactive } from 'vue';
-    const userInifo = reactive({
+    import { reactive, ref } from 'vue';
+    const userInifo = ref({
         telphone:'',
         password:''
-    });
+    })
     function login(){
         alert('点击了登录');
         console.log("账号是"+userInifo.telphone+'密码是'+userInifo.password)
